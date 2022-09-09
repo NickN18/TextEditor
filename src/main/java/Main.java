d7f4f8cdf2f056e01f07dea4082331a43c6d9b47
Nicolas Noriega <nicolasn8171@gmail.com>
2022-09-09 10:26:50 -0700

    added JNA library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Structure;
 
 public class Main
 {
@@ -10,5 +12,39 @@ public class Main
 
 interface LibC extends Library
 {
+    int SYSTEM_OUT = 0;
+    int ISIG = 1;
+    int ICANON = 2;
+    int ECHO = 10;
+    int TCSAFLUSH = 2;
+    int IXON = 2000;
+    int ICRNL = 400;
+    int IEXTEN = 100000;
+    int OPOST = 1;
+    int VMIN = 6;
+    int VTIME = 5;
+    int TIOCGWINSZ = 0x5413;
+
+    //Loading the C standard library for POSIX systems
+    LibC INSTANCE = Native.load("c", LibC.class);
+
+    @Structure.FieldOrder(value = {"c_iflag", "c_oflag", "c_cflag", "c_lflag", "c_cc"})
+    class Termios extends Structure
+    {
+        public int c_iflag, c_oflag, c_cflag, c_lflag;
+
+        public byte[] c_cc = new byte[19];
+
+        public Termios() {}
+
+
+
+    }
+
+    int tcgetattr(int fd, Termios termios);
+
+    int tcsetattr(int fd, int optionalActions, Termios termios);
+
+
 
 }
